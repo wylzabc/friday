47215ccb681ed94cc0c632aa8e10ee331b029d2d
wylzabc <wylzabc@gmail.com>
2019-06-01 11:25:18 +0800

    add absadd func

--- add/add.go
+++ add/add.go
@@ -17,3 +17,23 @@ func Add(c *gin.Context) {
 	fmt.Println(data)
 	c.JSON(http.StatusOK, gin.H{"result": data.Result})
 }
+
+func AbsAdd(c *gin.Context) {
+	var data data.Data
+	if err := c.ShouldBindJSON(&data); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	if data.Num1 > 0 && data.Num2 > 0 {
+		data.Result = data.Num1 + data.Num2
+	} else if data.Num1 > 0 && data.Num2 < 0 {
+		data.Result = data.Num1 + (-data.Num2)
+	} else if data.Num1 < 0 && data.Num2 > 0 {
+		data.Result = (-data.Num1) + data.Num2
+	} else {
+		data.Result = (-data.Num1) + (-data.Num2)
+	}
+
+	fmt.Println(data)
+	c.JSON(http.StatusOK, gin.H{"result": data.Result})
+}
--- add/add_test.go
+++ add/add_test.go
@@ -16,6 +16,7 @@ func Init() {
 	gin.SetMode(gin.DebugMode)
 	router = gin.Default()
 	router.POST("/add/add", Add)
+	router.POST("/add/absadd", AbsAdd)
 }
 
 func TestAdd(t *testing.T) {
@@ -34,4 +35,34 @@ func TestAdd(t *testing.T) {
 	if result.Result != 3 {
 		t.Errorf("响应数据不符，result: %v\n", result)
 	}
+}
+
+var datas = []data.Data{
+	{1, 2, 3},
+	{1, -2, 3},
+	{-1, 2, 3},
+	{-1, -2, 3},
+}
+
+func TestAbsAdd(t *testing.T) {
+	Init()
+	for _, testData := range datas {
+		uri := "/add/absadd"
+
+		param := make(map[string]interface{})
+		param["num1"] = testData.Num1
+		param["num2"] = testData.Num2
+
+		body := util.PostJson(uri, param, router)
+
+		var result data.Result
+		err := json.Unmarshal(body, &result)
+		if err != nil {
+			t.Errorf("解析响应出错，err:%v\n", err)
+		}
+		if result.Result != testData.Result {
+			t.Errorf("响应数据不符，result: %v\n", result)
+		}
+	}
+
 }
